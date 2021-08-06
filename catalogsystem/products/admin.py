d8691b68b1cd352a2a3c1563cd677646dd75c0c6
Sandra <sandrav0609@gmail.com>
2021-08-06 10:44:10 -0400

    add brand and product models add admin for both models

--- catalogsystem/products/admin.py
+++ catalogsystem/products/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Brand, Product
 
-# Register your models here.
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    """ Admin for brands """
+    list_display = ('id', 'name')
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """ Admin for products """
+    list_display = ('id', 'sku', 'name', 'price', 'brand', 'visits')
--- catalogsystem/products/models.py
+++ catalogsystem/products/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+    """ Brand model
+    
+    Attributes:
+    + name
+    """
+    name = models.CharField(
+        max_length=30,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    """ Product model
+
+    Attributes:
+    + sku
+    + name
+    + price
+    + brand
+    + visits
+    """
+    sku = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name="SKU"
+    )
+    name = models.CharField(
+        max_length=255
+    )
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    brand = models.ForeignKey(
+        Brand,
+        on_delete=models.CASCADE,
+        related_name="products"
+    )
+    visits = models.PositiveIntegerField(
+        default=0
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.sku})"
+
+    # def set_sku(self, sku):
+    #     """ Sets SKU new value 
+
+    #         Parameters:
+    #         + sku
+    #     """
+    #     self.sku = sku
+    #     self.save()
+
+    # def set_name(self, name):
+    #     """ Sets name new value 
+
+    #         Parameters:
+    #         + name
+    #     """
+    #     self.name = name
+    #     self.save()
+    
+    # def set_price(self, price):
+    #     """ Sets price new value 
+
+    #         Parameters:
+    #         + price
+    #     """
+    #     self.price = price
+    #     self.save()
+
+    def update_visits(self):
+        """ Updates visits count """
+        self.visits += 1
+        self.save()
