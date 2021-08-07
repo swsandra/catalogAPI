2aa88cde61eb9aa75f19abf50e508f5063a34089
Sandra <sandrav0609@gmail.com>
2021-08-07 13:57:35 -0400

    add utils.py with function for email notification

---
+++ catalogsystem/products/utils.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.core.mail import send_mail
+import rest_framework
+
+from .models import User, Brand, Product
+
+def send_email_notification(user, old_instance, instance, update=True):
+    """ Sends an email notification to all users except the one performing the change
+    
+        Parameters:
+        + user: User changing the instance
+        + old_instance: Old instance (updated or removed)
+        + instance: Instance with updated information
+        + update: whether the instance was updated or not (deleted)
+    """
+    if update and not instance.changed(old_instance): # Instance didn't change, notification is not necessary
+        return
+    # Subject construction
+    subject = ""
+    instance_type = "instance"
+    if isinstance(old_instance, Brand):
+        subject = "Brand"
+        instance_type = "brand"
+    elif isinstance(old_instance, Product):
+        subject = "Product"
+        instance_type = "product"
+    subject += f" {old_instance.name}" # Product/brand old name
+                                       # (if the name changed, admins can still identify it)
+    subject += " updated" if update else " deleted" # Action performed
+    # Message construction
+    message = f"{user.first_name} {user.last_name} ({user.username}) has "
+    if update:
+        message += f"updated the following fields:{instance.get_updated_info_str(old_instance)}"
+    else:
+        message += f"removed the {instance_type}.\n{old_instance.get_info_str()}"
+
+    recipient_list = User.objects.exclude(id=user.id).values_list('email', flat=True)
+    send_mail(
+        subject,
+        message,
+        settings.EMAIL_HOST_USER,
+        recipient_list
+    )
