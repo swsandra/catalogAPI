a356d1608cf940d75caa5551dd71f469d3f15e94
Sandra <sandrav0609@gmail.com>
2021-08-07 13:56:17 -0400

    add methods to get brand/product information and updated information

--- catalogsystem/products/models.py
+++ catalogsystem/products/models.py
@@ -28,6 +28,24 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
+    def get_info_str(self):
+        """ Returns brand information as a string """
+        return f"Brand information:\n\tname: {self.name}\n"
+    
+    def changed(self, old_self):
+        """ Compares self to old instance, field by field, to determine if
+            self has changed.
+            
+            Returns bool indicating if self has changed."""
+        return self.name != old_self.name
+    
+    def get_updated_info_str(self, old_self):
+        """ Returns updated information as a string """
+        info_str = ""
+        if self.name != old_self.name:
+            info_str += f"\n\tname: {old_self.name} -> {self.name}"
+        return info_str
+
 class Product(models.Model):
     """ Product model
 
@@ -66,3 +84,32 @@ class Product(models.Model):
         """ Updates visits count """
         self.visits += 1
         self.save()
+
+    def get_info_str(self):
+        """ Returns product information as a string """
+        return "Product information:\n" + \
+                f"\tSKU: {self.sku}\n" + \
+                f"\tname: {self.name}\n" + \
+                f"\tprice: {self.price}\n" + \
+                f"\tbrand: {self.brand}\n"
+    
+    def changed(self, old_self):
+        """ Compares self to old instance, field by field, to determine if
+            self has changed.
+            
+            Returns bool indicating if self has changed."""
+        return self.sku != old_self.sku or self.name != old_self.name \
+            or self.price != old_self.price or self.brand != old_self.brand
+
+    def get_updated_info_str(self, old_self):
+        """ Returns updated information as a string """
+        info_str = ""
+        if self.sku != old_self.sku:
+            info_str += f"\n\tSKU: {old_self.sku} -> {self.sku}"
+        if self.name != old_self.name:
+            info_str += f"\n\tname: {old_self.name} -> {self.name}"
+        if self.price != old_self.price:
+            info_str += f"\n\tprice: {old_self.price} -> {self.price}"
+        if self.brand != old_self.brand:
+            info_str += f"\n\tbrand: {old_self.brand} -> {self.brand}"
+        return info_str
