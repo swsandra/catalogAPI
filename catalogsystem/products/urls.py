5ef0ec798a015bbfaa87841f55d9a63f8af3cc0d
Sandra <sandrav0609@gmail.com>
2021-08-07 17:00:16 -0400

    change api root docstring

--- catalogsystem/products/urls.py
+++ catalogsystem/products/urls.py
@@ -9,6 +9,9 @@ router.register(r'users', UserViewSet, basename='user')
 router.register(r'brands', BrandViewSet, basename='brand')
 router.register(r'products', ProductViewSet, basename='product')
 
+router.get_api_root_view().cls.__doc__ = \
+    ("Api Root View.\n\nusers: Endpoint for users.\nbrands: Endpoint for brands.\nproducts: Endpoint for products.")
+
 urlpatterns = [
     path('api/users/change-password',
         ChangePasswordView.as_view(), name='change_password'),
