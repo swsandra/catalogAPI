e15f259ac39203fa380d00708364bacf0485e9b3
Sandra <sandrav0609@gmail.com>
2021-08-07 20:09:04 -0400

    fix email message for deleted instances

--- catalogsystem/products/utils.py
+++ catalogsystem/products/utils.py
@@ -23,15 +23,17 @@ def send_email_notification(user, old_instance, instance, update=True):
     elif isinstance(old_instance, Product):
         subject = "Product"
         instance_type = "product"
-    subject += f" {old_instance.name} with ID {old_instance.id}" # Product/brand old name
-                                       # (if the name changed, admins can still identify it)
+    subject += f" {old_instance.name}" # Product/brand old name (if the name changed,
+                                       # admins can still identify it)
+    if update:
+        subject += f" with ID {old_instance.id}"
     subject += " updated" if update else " deleted" # Action performed
     # Message construction
     message = f"{user.first_name} {user.last_name} ({user.username}) has "
     if update:
-        message += f"updated the product {old_instance.name} (ID: {old_instance.id}).\nFields updated:{instance.get_updated_info_str(old_instance)}"
+        message += f"updated the {instance_type} {old_instance.name} (ID: {old_instance.id}).\nFields updated:{instance.get_updated_info_str(old_instance)}"
     else:
-        message += f"removed the {instance_type}.\n{old_instance.get_info_str()}"
+        message += f"removed the {instance_type} {old_instance.name}.\n{old_instance.get_info_str()}"
 
     recipient_list = User.objects.exclude(id=user.id).values_list('email', flat=True)
     send_mail(
